8171c264684c4ce500e85d6de4acad9bc6c5c33c
HUSTwo <991097451@qq.com>
2020-08-14 00:12:46 +0800

    Data Visualization
    绘制折线图、散点图、直方图，进行核密度估计，绘制相关系数矩阵

--- DataVisualization.py
+++ DataVisualization.py
@@ -5,9 +5,7 @@ Created on Thu Aug 13 23:34:29 2020
 @author: U201812776
 """
 
-"""
-将数据从.scv文件中读取出来
-"""
+"""将数据从.scv文件中读取出来"""
 
 # 导入数据分析库pandas
 import pandas as pd
@@ -27,6 +25,56 @@ df.head()
 
 # 取出时间
 raw_time = pd.to_datetime(df.pop('Unnamed: 0'), format='%Y/%m/%d %H:%M')
+
+"""将数据进行可视化处理"""
+
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# 折线图：股票走势
+plt.plot(raw_time, df['close'])
+plt.xlabel('Time')
+plt.ylabel('Share Price')
+plt.title('Trend')
+plt.show()
+
+# 散点图：成交量和股价
+
+plt.scatter(df['volume'], df['close'])
+plt.xlabel('Volume')
+plt.ylabel('Share Price')
+plt.title('Volume & Share Price')
+plt.show()
+
+#切片取前300组数据
+plt.scatter(df['volume'][:300], df['close'][:300]) 
+plt.xlabel('Volume')
+plt.ylabel('Share Price')
+plt.title('Volume & Share Price')
+plt.show()
+
+# 涨跌幅度
+daily_return = df['close'][0::240].pct_change().dropna()
+plt.plot(raw_time[0::240][:40], daily_return[:40])
+plt.xlabel('Time')
+plt.ylabel('Rise and Fall')
+plt.show()
+
+# 直方图
+plt.hist(daily_return)
+
+# 核密度估计
+sns.kdeplot(daily_return)
+
+# 相关系数矩阵
+correlation = df.corr()
+print(correlation)
+
+sns.heatmap(correlation, annot=True)
+
+
+
+
 
 
 
